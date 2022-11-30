117a16e0ecd2b4ea4ec380393925a51d3e87c947
Davidlhevilla <92096052+Davidlhevilla@users.noreply.github.com>
2022-11-30 08:49:22 +0100

    Subida Ejercicio 2 con error corregido

--- Ejercicio2/src/module-info.java
+++ Ejercicio2/src/module-info.java
@@ -3,6 +3,12 @@ module Ejercicio2 {
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires javafx.base;
+	exports application;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	
+	
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+	
+	
+	
 }
